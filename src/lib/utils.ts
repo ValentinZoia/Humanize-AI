c309fabd7126aa59f04aca6828742ef3a4506b9c
ValentinZoia <valentinzoia@gmail.com>
2025-03-30 23:15:55 -0300

    add getEmailInitial and copyClipboard functions to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const getEmailInitial = (name: string) => {
+  return name.split(' ').map((n) => n[0] + n[1]).join('').toUpperCase();
+}
+
+export const copyClipboard = (text: string) => {
+  navigator.clipboard.writeText(text);
+}
