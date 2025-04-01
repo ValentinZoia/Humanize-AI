cd39e5c802ee0f8b790802630777d988c4536d7a
ValentinZoia <valentinzoia@gmail.com>
2025-03-31 23:16:19 -0300

    refactory middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,40 +9,40 @@ DEFAULT_LOGIN_REDIRECT,
 import { NextResponse } from "next/server";
 
 
-// const {auth} = NextAuth(authOptions);
+const {auth} = NextAuth(authOptions);
 
-// export default auth((req) => {
-//   const { nextUrl } = req;
-//   const { pathname } = nextUrl;
-//   const isLoggedIn = !!req.auth;
-//   const isPublic = publicRoutes.some((route) => route.path === pathname);
-//   const isAuth = authRoutes.some((route) => route.path === pathname);
-//   const isApi = apiAuthPrefix.some(prefix => pathname.startsWith(prefix.path));
+export default auth((req) => {
+  const { nextUrl } = req;
+  const { pathname } = nextUrl;
+  const isLoggedIn = !!req.auth;
+  const isPublic = publicRoutes.some((route) => route.path === pathname);
+  const isAuth = authRoutes.some((route) => route.path === pathname);
+  const isApi = apiAuthPrefix.some(prefix => pathname.startsWith(prefix.path));
 
-//   if (isApi) {
+  if (isApi) {
     
-//     return NextResponse.next();
-//   }
+    return NextResponse.next();
+  }
 
-//   // Redirect to / if authenticated and im in a auth route
-//   // Redirigir a / si estoy autenticado y estoy en una ruta de autenticacion
-//   if(isAuth){
-//     if(isLoggedIn){
-//       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT[0].path, req.url));
-//     }
-//     return NextResponse.next();
-//   }
+  // Redirect to / if i authenticated and im in a auth route
+  // Redirigir a / si estoy autenticado y estoy en una ruta de autenticacion
+  if(isAuth){
+    if(isLoggedIn){
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT[0].path, req.url));
+    }
+    return NextResponse.next();
+  }
 
   
-//   // Redirect to login if not authenticated and im not a public route
-//   // Redirigir a login si no estoy autenticado y no estoy en una ruta publica
-//   if(!isLoggedIn && !isPublic && !isApi){
-//     return NextResponse.redirect(new URL("/login", req.url));
-//   }
+  // Redirect to login if not authenticated and im not a public route
+  // Redirigir a login si no estoy autenticado y no estoy en una ruta publica
+  // if(!isLoggedIn && !isPublic && !isApi){
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
-//   return NextResponse.next();
+  return NextResponse.next();
   
-// })
+})
 
 
 export const config = {
